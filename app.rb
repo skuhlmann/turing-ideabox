424266f64c07cc59c2097a83e4db0d5736819d5a
Sam Kuhlmann <samkuhlmann@gmail.com>
2014-10-28 06:32:12 -0600

    Initial commit. Adding and deleting ideas

---
+++ app.rb
@@ -0,0 +1,33 @@
+require './idea'
+
+class IdeaBoxApp < Sinatra::Base
+	set :method_override, true
+
+	configure :development do
+    register Sinatra::Reloader
+  end
+
+	not_found do
+	  erb :error
+	end
+
+	get '/' do 
+		erb :index, locals: {ideas: Idea.all}
+	end
+
+	post '/' do
+		idea = Idea.new(params['idea_title'], params['idea_description'])
+		idea.save
+	  redirect '/'
+	end
+
+	delete '/:id' do |id|
+	  Idea.delete(id.to_i)
+	  redirect '/'
+	end
+
+	get '/:id/edit' do |id|
+		erb :edit
+	end
+
+end
---
+++ idea.rb
@@ -0,0 +1,43 @@
+require 'yaml/store'
+
+class Idea
+	attr_reader :title, :description
+
+	def initialize(title, description)
+		@title = title
+		@description = description
+	end
+
+	def save
+			database.transaction do |db|
+				db['ideas'] ||= []
+				db['ideas'] << {title: title, description: description}
+			end
+	end
+
+	def database
+		Idea.database
+	end
+
+	def self.database
+		@database ||= YAML::Store.new "ideabox"
+	end
+
+	def self.raw_ideas
+		database.transaction do |db|
+			db['ideas'] || []
+		end
+	end
+
+	def self.all
+		raw_ideas.map do |data|
+			new(data[:title], data[:description])
+		end
+	end
+
+	def self.delete(position)
+    database.transaction do
+      database['ideas'].delete_at(position)
+    end
+  end
+end
